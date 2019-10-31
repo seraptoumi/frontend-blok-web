e0b39d2a7a2fca65e6e9840cd9fdc32f74bcc33f
seraptoumi <55747674+seraptoumi@users.noreply.github.com>
2019-10-31 16:46:23 +0100

    Fotos toegevoegd en de pagina's gelinkt

---
+++ index.js
@@ -0,0 +1,43 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+//
+//var downloaden = document.querySelector(".vers");
+//
+//function vinkje() {
+//    downloaden.classList.replace("fa-arrow-down", "fa-check");
+//}
+//
+//downloaden.addEventListener("click", vinkje);
+
+var vervangen = document.querySelectorAll(".vers");
+var zoekIcon = document.querySelector(".fa-search");
+var zoekBalk = document.querySelector("#zoek");
+var i;
+
+function vinkje() {
+
+
+    if (this.classList[1] == "fa-arrow-down") {
+        this.classList.replace("fa-arrow-down", "fa-check");
+    } else {
+        this.classList.replace("fa-check", "fa-arrow-down");
+    }
+}
+
+for (i = 0; i < vervangen.length; i++) {
+    vervangen[i].addEventListener('click', vinkje);
+}
+
+
+function balk() {
+    if( zoekBalk.style.opacity === "0") {
+        zoekBalk.style.opacity = "1";
+    } else {
+        zoekBalk.style.opacity = "0";
+    }
+
+}
+
+zoekIcon.addEventListener("click", balk);
+
