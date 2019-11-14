c3042dc2c7f4caf6d424c0d898b09f5f1548e21e
seraptoumi <55747674+seraptoumi@users.noreply.github.com>
2019-11-14 18:13:10 +0100

    aanvulling beoordeling
    focus toegevoegd, <hr> weggehaald en microinteractie bij home pagina klikken op downloaden -->vinkje + bij profiel er eentje bij als feedback

--- index.js
+++ index.js
@@ -3,23 +3,31 @@
 /*eslint 'no-console':0*/
 //met hulp van kaan
 
-var vervangen = document.querySelectorAll(".vers");
+var vervangKnop = document.querySelectorAll("button"); // pakt alle knoppen uit html
 var zoekIcon = document.querySelector(".fa-search");
 var zoekBalk = document.querySelector("#zoek");
+var downloads = document.querySelector("#downloads"); // pakt downloadteller rechtsboven bij profielicoon
+downloads.style.display = "none"; // zet het downloadteller op hidden
 var i;
 
 function vinkje() {
 
-
-    if (this.classList[1] == "fa-arrow-down") {
-        this.classList.replace("fa-arrow-down", "fa-check");
+    var vervangIcon = this.querySelector('i'); // pakt de icoon van het knop
+    if (vervangIcon.classList[1] == "fa-arrow-down") { // deze 'if-statement' vervangt de icoon met het 'check' icoon
+        vervangIcon.classList.replace("fa-arrow-down", "fa-check");
+        this.classList.add('selected'); // class toevoegen voor animatie
+        downloads.style.display = "unset"; // zet het downloadteller op visible
+        downloads.innerHTML++; // Doet +1 op het downloadteller 
     } else {
-        this.classList.replace("fa-check", "fa-arrow-down");
+        vervangIcon.classList.replace("fa-check", "fa-arrow-down");
+        this.classList.remove('selected');
+        downloads.innerHTML--; // Doet weer -1 op het downloadteller
     }
+
 }
 
-for (i = 0; i < vervangen.length; i++) {
-    vervangen[i].addEventListener('click', vinkje);
+for (i = 0; i < vervangKnop.length; i++) {
+    vervangKnop[i].addEventListener('click', vinkje); // zet hetzelfde functie nu op de knop inplaats van de icon
 }
 
 
